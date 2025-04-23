9fe3e5c035f28a652c9b33c6a35d0fcc54511bdf
Lamuroff <milanlalince@gmail.com>
2025-04-23 15:33:18 +0300

    Add files via upload

---
+++ UNBAN-SERVICE.cmd
@@ -0,0 +1,13 @@
+start "zapret smotra: http,https,quic" /min "%~dp0winws.exe" ^
+--wf-tcp=80,443,22006,23006,24006 --wf-udp=443,22005,50000-50099 ^
+--filter-tcp=80 --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=443 --hostlist="%~dp0files\list-80443.txt" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --new ^
+--filter-tcp=443 --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=6 --dpi-desync-fooling=badseq,md5sig --new ^
+--filter-udp=443 --hostlist="%~dp0files\list-80443.txt" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="%~dp0files\quic_initial_www_google_com.bin" --new ^
+--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 --new ^
+--filter-udp=50000-50099 --filter-l7=discord,stun --dpi-desync=fake
+--filter-tcp=22006 --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=23006 --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=24006 --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=2052 --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-udp=22005 --dpi-desync=fake --dpi-desync-repeats=11 --new
